bfae40e1e20f1eef7ee4b0c34f050e6c2d3c607c
HamidReza <hr.m.szh@gmail.com>
2022-09-28 11:59:37 +0330

    Add chat page demo

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from .views import Register, Logout
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenObtainPairView,
+    TokenVerifyView,
 )
 
 app_name = "accounts"
@@ -14,4 +15,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # get jwt token
     path('logout/', Logout.as_view(), name='logout'),  # user logout with jwt
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # jwt refresh token
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # verify jwt token
 ]
