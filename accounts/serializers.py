e2db462c43ccda9b5b77fd0c268ac9f199eb1244
HamidReza <hr.m.szh@gmail.com>
2022-10-01 23:08:59 +0330

    Add disposable_email_domains

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import User
+from disposable_email_domains import blocklist
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,6 +19,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         if validated_data['password'] != validated_data['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        elif validated_data['email'].split('@')[1] in blocklist:
+            raise serializers.ValidationError({"email": "The email domain is blocked."})
 
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
